0cca8433daf324edfc2fe76cbc758de3f71857dc
Axel <Axel@DESKTOP-NJ4PENC.home>
2021-12-23 17:14:36 +0100

    Informations sur les devoirs rendus

---
+++ 21103268-Programmation-Objet-et-Goupware/src/tp08/InfoDevoirs.java
@@ -0,0 +1,13 @@
+package tp08;
+
+public class InfoDevoirs {
+
+}
+
+/**Informations sur les différents devoirs :
+ * Devoir 1 dans TP2
+ * Devoir 2 dans TP4
+ * Devoir 3 dans TP5
+ * Devoir 4 dans TP6
+ * Devoir 5 dans TP7
+ * Devoir 6 dans TP8*/
